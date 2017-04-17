d00a88ba6f5cdef713fe361c3dd73940e11a2408
sparkecho <echozhz@126.com>
2017-04-18 01:27:25 +0800

    Add C union type sdl-event, fixed many bugs

--- events.lisp
+++ events.lisp
@@ -19,11 +19,11 @@
 (defconstant sdl-release 0 "General keyboard/mouse state definitions")
 (defconstant sdl-pressed 1 "General keyboard/mouse state definitions")
 
-;; 54
+
 (defcenum sdl-event-type
     (:SDL_FIRSTEVENT     0)             ; Unused (do not remove)
     ;; Application events
-    (:SDL_QUIT           0x100)         ; User-requested quit
+    (:SDL_QUIT           #x100)         ; User-requested quit
     ;; These application events have special meaning on iOS, see README-ios.txt for details
     (:SDL_APP_TERMINATING) ; The application is being terminated by the OS
                                         ; Called on iOS in applicationWillTerminate()
@@ -46,23 +46,23 @@
                                         ; Called on iOS in applicationDidBecomeActive()
                                         ; Called on Android in onResume()
     ;; Window events
-    (:SDL_WINDOWEVENT    0x200)         ; Window state change
+    (:SDL_WINDOWEVENT    #x200)         ; Window state change
     (:SDL_SYSWMEVENT)                   ; System specific event
 
     ;; Keyboard events
-    (:SDL_KEYDOWN        0x300)  ; Key pressed
+    (:SDL_KEYDOWN        #x300)  ; Key pressed
     (:SDL_KEYUP)                 ; Key released
     (:SDL_TEXTEDITING)           ; Keyboard text editing (composition)
     (:SDL_TEXTINPUT)             ; Keyboard text input
 
     ;; Mouse events
-    (:SDL_MOUSEMOTION    0x400)         ; Mouse moved
+    (:SDL_MOUSEMOTION    #x400)         ; Mouse moved
     (:SDL_MOUSEBUTTONDOWN)              ; Mouse button pressed
     (:SDL_MOUSEBUTTONUP)                ; Mouse button released
     (:SDL_MOUSEWHEEL)                   ; Mouse wheel motion
 
     ;; Joystick events
-    (:SDL_JOYAXISMOTION  0x600)         ; Joystick axis motion
+    (:SDL_JOYAXISMOTION  #x600)         ; Joystick axis motion
     (:SDL_JOYBALLMOTION)                ; Joystick trackball motion
     (:SDL_JOYHATMOTION)                 ; Joystick hat position change
     (:SDL_JOYBUTTONDOWN)                ; Joystick button pressed
@@ -71,7 +71,7 @@
     (:SDL_JOYDEVICEREMOVED)             ; An opened joystick has been removed
 
     ;; Game controller events
-    (:SDL_CONTROLLERAXISMOTION  0x650)  ; Game controller axis motion
+    (:SDL_CONTROLLERAXISMOTION  #x650)  ; Game controller axis motion
     (:SDL_CONTROLLERBUTTONDOWN)      ; Game controller button pressed
     (:SDL_CONTROLLERBUTTONUP)        ; Game controller button released
     (:SDL_CONTROLLERDEVICEADDED) ; A new Game controller has been inserted into the system
@@ -79,48 +79,97 @@
     (:SDL_CONTROLLERDEVICEREMAPPED) ; The controller mapping was updated
 
     ;; Touch events
-    (:SDL_FINGERDOWN      0x700)
+    (:SDL_FINGERDOWN      #x700)
     (:SDL_FINGERUP)
     (:SDL_FINGERMOTION)
 
     ;; Gesture events
-    (:SDL_DOLLARGESTURE   0x800)
+    (:SDL_DOLLARGESTURE   #x800)
     (:SDL_DOLLARRECORD)
     (:SDL_MULTIGESTURE)
 
     ;; Clipboard events
-    (:SDL_CLIPBOARDUPDATE 0x900)        ; The clipboard changed
+    (:SDL_CLIPBOARDUPDATE #x900)        ; The clipboard changed
 
     ;; Drag and drop events
-    (:SDL_DROPFILE        0x1000)    ; The system requests a file open
+    (:SDL_DROPFILE        #x1000)    ; The system requests a file open
 
     ;; Render events
-    (:SDL_RENDER_TARGETS_RESET 0x2000) ; The render targets have been reset
+    (:SDL_RENDER_TARGETS_RESET #x2000) ; The render targets have been reset
 
     ;; Events ::SDL_USEREVENT through ::SDL_LASTEVENT are for your use,
     ;; and should be allocated with SDL_RegisterEvents()
-    (:SDL_USEREVENT    0x8000)
+    (:SDL_USEREVENT    #x8000)
 
     ;; This last event is only for bounding internal arrays
-    (:SDL_LASTEVENT    0xFFFF))
-;; 150
+    (:SDL_LASTEVENT    #xFFFF))
 
-;; brief Fields shared by every event
-(defcstruct sdl-common-event-struct
+
+;; Fields shared by every event
+(defcstructype sdl-common-event
   (type      :uint32)
   (timestamp :uint32))
 
-(defctype sdl-common-event (:struct sdl-common-event-struct))
 
-typedef struct SDL_WindowEvent
-{
-    Uint32 type;        /**< ::SDL_WINDOWEVENT */
-    Uint32 timestamp;
-    Uint32 windowID;    /**< The associated window */
-    Uint8 event;        /**< ::SDL_WindowEventID */
-    Uint8 padding1;
-    Uint8 padding2;
-    Uint8 padding3;
-    Sint32 data1;       /**< event dependent data */
-    Sint32 data2;       /**< event dependent data */
-} SDL_WindowEvent;
+;; Window state change event data (event.window.*)
+(defcstructype sdl-window-event
+  (type :uint32)                        ; ::SDL_WINDOWEVENT
+  (timestamp :uint32)
+  (windowID :uint32)                    ; The associated window
+  (event :uint8)                        ; ::SDL_WindowEventID
+  (padding1 :uint8)
+  (padding2 :uint8)
+  (padding3 :uint8)
+  (data1 :int32)                        ; event dependent data
+  (data2 :int32))                       ; event dependent data
+;; line 175
+
+
+;; line 449
+;; The "quit requested" event
+(defcstructype sdl-quit-event
+  (type      :uint32)                   ; ::SDL_QUIT
+  (timestamp :uint32))
+;; line 455
+
+
+;; line 498
+(defcuniontype sdl-event
+    (type     :uint32)                  ; Event type, shared with all events
+    (common   sdl-common-event)         ; Common event data
+    (window   sdl-window-event)         ; Window event data
+    ;; (key      SDL_KeyboardEvent)        ; Keyboard event data
+    ;; (edit     SDL_TextEditingEvent)     ; Text editing event data
+    ;; (text     SDL_TextInputEvent)       ; Text input event data
+    ;; (motion   SDL_MouseMotionEvent)     ; Mouse motion event data
+    ;; (button   SDL_MouseButtonEvent)     ; Mouse button event data
+    ;; (wheel    SDL_MouseWheelEvent)      ; Mouse wheel event data
+    ;; (jaxis    SDL_JoyAxisEvent)         ; Joystick axis event data
+    ;; (jball    SDL_JoyBallEvent)         ; Joystick ball event data
+    ;; (jhat     SDL_JoyHatEvent)          ; Joystick hat event data
+    ;; (jbutton  SDL_JoyButtonEvent)       ; Joystick button event data
+    ;; (jdevice  SDL_JoyDeviceEvent)       ; Joystick device change event data
+    ;; (caxis    SDL_ControllerAxisEvent)  ; Game Controller axis event data
+    ;; (cbutton  SDL_ControllerButtonEvent); Game Controller button event data
+    ;; (cdevice  SDL_ControllerDeviceEvent); Game Controller device event data
+    (quit     sdl-quit-event)           ; Quit request event data
+    ;; (user     SDL_UserEvent)            ; Custom event data
+    ;; (syswm    SDL_SysWMEvent)           ; System dependent window event data
+    ;; (tfinger  SDL_TouchFingerEvent)     ; Touch finger event data
+    ;; (mgesture SDL_MultiGestureEvent)    ; Gesture event data
+    ;; (dgesture SDL_DollarGestureEvent)   ; Gesture event data
+    ;; (drop     SDL_DropEvent)            ; Drag and drop event data
+
+    ;; This is necessary for ABI compatibility between Visual C++ and GCC
+    ;; Visual C++ will respect the push pack pragma and use 52 bytes for
+    ;; this structure, and GCC will use the alignment of the largest datatype
+    ;; within the union, which is 8 bytes.
+    ;; So... we'll add padding to force the size to be 56 bytes for both.
+    (padding :uint8 :count 56))
+;; line 554
+
+
+;; line 598
+(defcfun ("SDL_PollEvent" sdl-poll-event) :int
+  (event :pointer))                     ;(SDL_Event *)
+;; line 599
