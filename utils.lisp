db311fb9e4b0829e2586bc0856e36ffd672ca733
sparkecho <echozhz@126.com>
2017-04-18 23:44:04 +0800

    Extend with auto defined accessor to struct

--- utils.lisp
+++ utils.lisp
@@ -11,11 +11,26 @@
 ;; } struct-name;
 (defmacro defcstructype (name-and-options &body fields)
   (destructuring-bind (name . options)
-      (cffi::ensure-list name-and-options)
-    (let ((%name (symbol-combine '% name)))
-      `(progn
-         (defcstruct ,%name ,@options ,@fields)
-         (defctype ,name (:struct ,%name))))))
+      (ensure-list name-and-options)
+    (let ((%name (symbol-combine '% name))
+          (conc-name (or (getf options :conc-name)
+                         (symbol-combine name '-))))
+      `(eval-when (:compile-toplevel :load-toplevel :execute)
+         (defcstruct (,%name ,@options) ,@fields)
+         (defctype ,name (:struct ,%name))
+         ,@(generate-struct-accessors %name conc-name
+                                      (mapcar #'car fields))
+         ',name))))         
+
+
+(defun generate-struct-accessors (name conc-name slot-names)
+  (loop with pointer-arg = (symbol-combine '#:pointer-to- name)
+        for slot in slot-names
+        for accessor = (symbol-combine conc-name slot)
+        collect `(defun ,accessor (,pointer-arg)
+                   (foreign-slot-value ,pointer-arg '(:struct ,name) ',slot))
+        collect `(defun (setf ,accessor) (value ,pointer-arg)
+                   (setf (foreign-slot-value ,pointer-arg '(:struct ,name) ',slot) value))))
 
 
 
@@ -27,8 +42,8 @@
 ;; } union-name;
 (defmacro defcuniontype (name-and-options &body fields)
   (destructuring-bind (name &key size)
-      (cffi::ensure-list name-and-options)
-    (let ((%name (symbol-combine '% name)))
+      (ensure-list name-and-options)
+    (let ((%name (symbol-combine '% name))
           (options (if (null size)
                        nil
                        `(:size ,size))))
@@ -47,3 +62,10 @@
     (if sym
         (values sym :internal)
         (intern name))))
+
+
+(defun ensure-list (list)
+  "If LIST is a list, it is returned. Otherwise returns the list designated by LIST."
+  (if (listp list)
+      list
+      (list list)))
